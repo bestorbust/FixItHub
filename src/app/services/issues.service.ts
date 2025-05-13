26513c92021c87b2e6543ff7ec9624b705e4ea69
priyadarshini_P <155546102+bestorbust@users.noreply.github.com>
2025-05-13 18:03:28 +0530

    Update issues.service.ts

--- src/app/services/issues.service.ts
+++ src/app/services/issues.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class IssuesService {
-  private apiUrl = 'https://fixithubbackend.onrender.com/';
+  private apiUrl = 'https://fixithubbackend.onrender.com';
 
   constructor(private http: HttpClient) {}
 
